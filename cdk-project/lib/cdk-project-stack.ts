703c2d8c6513236b98ade1eb95279094eeecf084
apiruthi <piruthiviraj.a@legalaid.nsw.gov.au>
2023-12-12 18:20:16 +0530

    Lambda-handler

--- cdk-project/lib/cdk-project-stack.ts
+++ cdk-project/lib/cdk-project-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { LambdaFnConstruct } from './lambda-construct';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkProjectStack extends cdk.Stack {
@@ -7,10 +9,16 @@ export class CdkProjectStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, 'MyTempFileBucket', {
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    })
 
     // example resource
     // const queue = new sqs.Queue(this, 'CdkProjectQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    new LambdaFnConstruct(this, "LambdaFnConstruct", { LambfnName: "LambdaFn" });
   }
 }
